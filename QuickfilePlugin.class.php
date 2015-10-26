052e9c205f44ef55648a4c6dcd23bd29b4a6727b
Florian Bieringer <florian.bieringer@uni-passau.de>
2015-10-26 14:18:08 +0100

    Code cleanup and user search

--- QuickfilePlugin.class.php
+++ QuickfilePlugin.class.php
@@ -9,6 +9,8 @@
  * @version 0.1a
  */
 class QuickfilePlugin extends StudIPPlugin implements SystemPlugin {
+    
+    const USER_SEARCH = false;
 
     public function __construct() {
         parent::__construct();
@@ -19,25 +21,47 @@ class QuickfilePlugin extends StudIPPlugin implements SystemPlugin {
 
     public function find_action() {
         $search = studip_utf8decode(Request::get('search'));
+        $query = "%$search%";
         
         // Filter for own courses
         if (!$GLOBALS['perm']->have_perm('admin')) {
-            $user_id = User::findCurrent()->id;
-            $ownseminars = "JOIN seminar_user ON (dokumente.seminar_id = seminar_user.seminar_id AND seminar_user.user_id = '{$user_id}') ";
+            $ownseminars = "JOIN seminar_user ON (dokumente.seminar_id = seminar_user.seminar_id AND seminar_user.user_id = :userid) ";
+        }
+        
+        if (self::USER_SEARCH) {
+            $usersearch = "JOIN auth_user_md5 ON (dokumente.user_id = auth_user_md5.user_id)";
+            $usercondition =  " OR CONCAT_WS(' ', auth_user_md5.vorname, auth_user_md5.nachname, auth_user_md5.vorname) = :prequery ";
         }
 
         // Now check if we got a seminar
         if (strpos($search, '/') !== FALSE) {
             $args = explode('/', $search);
+            $prequery = "%{$args[0]}%";
+            $query = "%{$args[1]}%";
             $binary = '%' . join('%', str_split(strtoupper($args[0]))) . '%';
-            $sql = "SELECT dokumente.* FROM dokumente JOIN seminare USING (seminar_id) $ownseminars WHERE (seminare.name LIKE BINARY '{$binary}' OR seminare.name LIKE '%{$args[0]}%')  AND dokumente.name LIKE '%{$args[1]}%' ORDER BY chdate DESC LIMIT 100";
+            $comp = "AND";
         } else {
-            $sql = "SELECT dokumente.* FROM dokumente JOIN seminare USING (seminar_id) $ownseminars WHERE dokumente.name LIKE '%{$search}%' OR seminare.name LIKE '%{$search}%' ORDER BY chdate DESC LIMIT 1000";
+            $prequery = $query;
+            $comp = "OR";
+            $binary = '%' . join('%', str_split(strtoupper($search))) . '%';
         }
-
+        
+        // Build query
+        $sql = "SELECT dokumente.* FROM dokumente "
+                . "JOIN seminare USING (seminar_id) $ownseminars $usersearch "
+                . "WHERE (seminare.name LIKE BINARY :binary OR seminare.name LIKE :prequery $usercondition) "
+                . "$comp dokumente.name LIKE :query "
+                . "ORDER BY chdate DESC LIMIT 100";
+        
         // now query
         $db = DBManager::get();
         $stmt = $db->prepare($sql);
+        if (!$GLOBALS['perm']->have_perm('admin')) {
+            $stmt->bindParam(':userid', User::findCurrent()->id);
+        }
+        $stmt->bindParam(':prequery', $prequery);
+        $stmt->bindParam(':query', $query);
+        $stmt->bindParam(':binary', $binary);
         $stmt->execute();
         $result = array();
         while (sizeof($result) < 5 && $data = $stmt->fetch(PDO::FETCH_ASSOC)) {
@@ -47,7 +71,7 @@ class QuickfilePlugin extends StudIPPlugin implements SystemPlugin {
                     'id' => $file->id,
                     'name' => $file->name,
                     'filename' => $file->filename,
-                    'course' => $file->course ? $file->course->getFullname() : '',
+                    'course' => $file->course ? $file->course->getFullname().(self::USER_SEARCH ? " ({$file->author->getFullname()})" : '') : '',
                     'date' => strftime('%x', $file->chdate)
                 );
             }
