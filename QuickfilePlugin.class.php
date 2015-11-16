fd34ff8ae5db8c246fe3dead18f7f0c31eb84714
Florian Bieringer <florian.bieringer@uni-passau.de>
2015-11-16 14:16:19 +0100

    Added my courses in search

--- QuickfilePlugin.class.php
+++ QuickfilePlugin.class.php
@@ -16,6 +16,16 @@ class QuickfilePlugin extends StudIPPlugin implements SystemPlugin
         self::addStylesheet('/assets/style.less');
         PageLayout::addScript($this->getPluginURL() . '/assets/quickfile.js');
         PageLayout::addBodyElements('<div id="quickfilewrapper"><div id="quickfile"><h3>' . _('Podium') . '</h3><div id="quickfileinput"><input type="text" placeholder="' . _('Veranstaltung / Datei') . '"></div><ul id="quickfilelist"></ul></div></div>');
+        $this->types['mycourses'] = array(
+            'name' => _('Meine Veranstaltungen'),
+            'sql' => array('QuickfilePlugin', 'search_mycourse'),
+            'filter' => array('QuickfilePlugin', 'filter_mycourse')
+        );/*
+        $this->types['user'] = array(
+            'name' => _('Veranstaltungen'),
+            'sql' => array('QuickfilePlugin', 'search_courses'),
+            'filter' => array('QuickfilePlugin', 'filter_courses')
+        );*/
         $this->types['user'] = array(
             'name' => _('Benutzer'),
             'sql' => array('QuickfilePlugin', 'search_user'),
@@ -25,80 +35,6 @@ class QuickfilePlugin extends StudIPPlugin implements SystemPlugin
             'name' => _('Datei'),
             'sql' => array('QuickfilePlugin', 'search_files'),
             'filter' => array('QuickfilePlugin', 'filter_file')
-        );
-    }
-
-
-    public function search_files($search)
-    {
-        // Filter for own courses
-        if (!$GLOBALS['perm']->have_perm('admin')) {
-            if (!$GLOBALS['perm']->have_perm('admin')) {
-                $user = DBManager::get()->quote(User::findCurrent()->id);
-            }
-            $ownseminars = "JOIN seminar_user ON (dokumente.seminar_id = seminar_user.seminar_id AND seminar_user.user_id = $user) ";
-        }
-
-        if (self::USER_SEARCH) {
-            $usersearch = "JOIN auth_user_md5 ON (dokumente.user_id = auth_user_md5.user_id)";
-            $usercondition = " OR CONCAT_WS(' ', auth_user_md5.vorname, auth_user_md5.nachname, auth_user_md5.vorname) = :prequery ";
-        }
-
-        // Now check if we got a seminar
-        if (strpos($search, '/') !== FALSE) {
-            $args = explode('/', $search);
-            $prequery = DBManager::get()->quote("%" . trim($args[0]) . "%");
-            $query = DBManager::get()->quote("%" . trim($args[1]) . "%");
-            $binary = DBManager::get()->quote('%' . join('%', str_split(strtoupper(trim($args[0])))) . '%');
-            $comp = "AND";
-        } else {
-            $query = DBManager::get()->quote("%$search%");
-            $prequery = $query;
-            $comp = "OR";
-            $binary =  DBManager::get()->quote('%'.join('%', str_split(strtoupper($search))) . '%');
-        }
-
-        // Build query
-        $sql = "SELECT 'file' as type, dokumente.dokument_id as id FROM dokumente "
-            . "JOIN seminare USING (seminar_id) $ownseminars $usersearch "
-            . "WHERE (seminare.name LIKE BINARY $binary OR seminare.name LIKE $prequery $usercondition) "
-            . "$comp dokumente.name LIKE $query "
-            . "ORDER BY dokumente.chdate DESC";
-        return $sql;
-    }
-
-    public function filter_file($file_id, $search)
-    {
-        $file = StudipDocument::find($file_id);
-        if ($file->checkAccess(User::findCurrent()->id)) {
-            return array(
-                'id' => $file->id,
-                'name' => self::mark($file->name, $search),
-                'url' => URLHelper::getURL("sendfile.php?type=0&file_id={$file->id}&file_name={$file->filename}"),
-                'additional' => self::mark($file->course ? $file->course->getFullname() . (self::USER_SEARCH ? " ({$file->author->getFullname()})" : '') : '', $search, false),
-                'date' => strftime('%x', $file->chdate)
-            );
-        }
-    }
-
-    public function search_user($search) {
-
-        if (!$search) {
-            return null;
-        }
-
-        $query = DBManager::get()->quote("%$search%");
-        $sql = "SELECT 'user' as type, user.user_id as id FROM auth_user_md5 user WHERE user.nachname LIKE $query OR username LIKE $query AND ".get_vis_query('user');
-        return $sql;
-    }
-
-    public function filter_user($user_id, $search) {
-        $user = User::find($user_id);
-        return array(
-            'id' => $user->id,
-            'name' => self::mark($user->getFullname(), $search),
-            'url' => URLHelper::getURL("dispatch.php/profile", array('username' => $user->username)),
-            'additional' => self::mark($user->username, $search)
         );
     }
 
@@ -145,6 +81,14 @@ class QuickfilePlugin extends StudIPPlugin implements SystemPlugin
         die;
     }
 
+    /**
+     * Function to mark a querystring in a resultstring
+     *
+     * @param $string
+     * @param $query
+     * @param bool|true $filename
+     * @return mixed
+     */
     public static function mark($string, $query, $filename = true)
     {
         if (strpos($query, '/') !== FALSE) {
@@ -177,6 +121,108 @@ class QuickfilePlugin extends StudIPPlugin implements SystemPlugin
             return $result;
         }
         return $string;
+    }
+
+    /**
+     * Returns SQL for the file search
+     *
+     * @param $search
+     * @return string
+     */
+    public function search_files($search)
+    {
+        // Filter for own courses
+        if (!$GLOBALS['perm']->have_perm('admin')) {
+            if (!$GLOBALS['perm']->have_perm('admin')) {
+                $user = DBManager::get()->quote(User::findCurrent()->id);
+            }
+            $ownseminars = "JOIN seminar_user ON (dokumente.seminar_id = seminar_user.seminar_id AND seminar_user.user_id = $user) ";
+        }
+
+        if (self::USER_SEARCH) {
+            $usersearch = "JOIN auth_user_md5 ON (dokumente.user_id = auth_user_md5.user_id)";
+            $usercondition = " OR CONCAT_WS(' ', auth_user_md5.vorname, auth_user_md5.nachname, auth_user_md5.vorname) = :prequery ";
+        }
+
+        // Now check if we got a seminar
+        if (strpos($search, '/') !== FALSE) {
+            $args = explode('/', $search);
+            $prequery = DBManager::get()->quote("%" . trim($args[0]) . "%");
+            $query = DBManager::get()->quote("%" . trim($args[1]) . "%");
+            $binary = DBManager::get()->quote('%' . join('%', str_split(strtoupper(trim($args[0])))) . '%');
+            $comp = "AND";
+        } else {
+            $query = DBManager::get()->quote("%$search%");
+            $prequery = $query;
+            $comp = "OR";
+            $binary = DBManager::get()->quote('%' . join('%', str_split(strtoupper($search))) . '%');
+        }
+
+        // Build query
+        $sql = "SELECT 'file' as type, dokumente.dokument_id as id FROM dokumente "
+            . "JOIN seminare USING (seminar_id) $ownseminars $usersearch "
+            . "WHERE (seminare.name LIKE BINARY $binary OR seminare.name LIKE $prequery $usercondition) "
+            . "$comp dokumente.name LIKE $query "
+            . "ORDER BY dokumente.chdate DESC";
+        return $sql;
+    }
+
+    public function filter_file($file_id, $search)
+    {
+        $file = StudipDocument::find($file_id);
+        if ($file->checkAccess(User::findCurrent()->id)) {
+            return array(
+                'id' => $file->id,
+                'name' => self::mark($file->name, $search),
+                'url' => URLHelper::getURL("sendfile.php?type=0&file_id={$file->id}&file_name={$file->filename}"),
+                'additional' => self::mark($file->course ? $file->course->getFullname() . (self::USER_SEARCH ? " ({$file->author->getFullname()})" : '') : '', $search, false),
+                'date' => strftime('%x', $file->chdate)
+            );
+        }
+    }
+
+    public function search_user($search)
+    {
+        if (!$search) {
+            return null;
+        }
+
+        $query = DBManager::get()->quote("%$search%");
+        $sql = "SELECT 'user' as type, user.user_id as id FROM auth_user_md5 user WHERE user.nachname LIKE $query OR username LIKE $query AND " . get_vis_query('user');
+        return $sql;
+    }
+
+    public function filter_user($user_id, $search)
+    {
+        $user = User::find($user_id);
+        return array(
+            'id' => $user->id,
+            'name' => self::mark($user->getFullname(), $search),
+            'url' => URLHelper::getURL("dispatch.php/profile", array('username' => $user->username)),
+            'additional' => self::mark($user->username, $search)
+        );
+    }
+
+    public function search_mycourse($search)
+    {
+        if (!$search) {
+            return null;
+        }
+        $query = DBManager::get()->quote("%$search%");
+        $user_id = DBManager::get()->quote(User::findCurrent()->id);
+        $sql = "SELECT 'mycourses' as type, courses.seminar_id as id FROM seminare courses JOIN seminar_user USING (seminar_id) WHERE user_id = $user_id AND courses.Name LIKE $query ORDER BY start_time DESC";
+        return $sql;
+    }
+
+    public function filter_mycourse($course_id, $search)
+    {
+        $course = Course::find($course_id);
+        return array(
+            'id' => $course->id,
+            'name' => self::mark($course->getFullname(), $search),
+            'url' => URLHelper::getURL("dispatch.php/course/details", array('cid' => $course->id)),
+            'date' => $course->start_semester->name
+        );
     }
 
 }
